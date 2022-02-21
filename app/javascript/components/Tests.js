4c8682f448297f299256c1619d4cc09c797d0ce1
jonquiiillll <jonquiiillll@gmail.com>
2022-02-21 10:48:23 +0300

    Создала новую ветку

---
+++ app/javascript/components/Tests.js
@@ -0,0 +1,18 @@
+import React from "react"
+import PropTypes from "prop-types"
+import A_Textblock from "./A_Textblock.js"
+
+class Tests extends React.Component {
+  render () {
+    return (
+      <div>
+        <h1>
+          Tests
+        </h1>
+        <A_Textblock />
+      </div>
+    );
+  }
+}
+
+export default Tests
---
+++ app/javascript/components/Users.js
@@ -0,0 +1,23 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+class Users extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>All Users</h1>
+        <ul>
+          {this.props.users.map(user => (
+            <li key={user.id}>{`${user.id} ${user.username} ${user.email} ${user.isadmin}`}</li>
+          ))}
+        </ul>
+
+      </div>
+    );
+  }
+}
+
+Users.propTypes = {
+  users: PropTypes.array
+};
+export default Users
